0a7ab5ada4e77f12c35dd13cb7699b41eb3af4c6
Pankaj Gupta <guptapankaj15@outlook.com>
2023-12-27 15:25:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 const db = require('./config/mongoose');
 const path = require('path');
 const expressLayouts = require("express-ejs-layouts");
@@ -25,11 +25,9 @@ app.set('views', './views');
 app.use('/', require('./routes'));
 
 
-app.listen(port,function(err){
-    if(err){
-        console.log("Error in running the server ",err);
-        return;
+app.listen(port, function (err) {
+    if (err) {
+      console.log(`Error in running the server: ${err}`);
     }
-
-    console.log("Server is up and running ");
-});
+    console.log(`Server is running on port: ${port}`);
+  });
